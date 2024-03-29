5f09081f5713f6990802e8e6a75f444ff8b311aa
SANTIAGO LOPEZ <santiagolopezslz60@gmail.com>
2024-03-28 21:30:00 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace pryLopezSantiagoLab2
+{
+    partial class frmGrilla
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvInformacion = new System.Windows.Forms.DataGridView();
+            this.txtDni = new System.Windows.Forms.TextBox();
+            this.txtSector = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblSector = new System.Windows.Forms.Label();
+            this.lblDni = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInformacion)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvInformacion
+            // 
+            this.dgvInformacion.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInformacion.Location = new System.Drawing.Point(262, 26);
+            this.dgvInformacion.Name = "dgvInformacion";
+            this.dgvInformacion.RowHeadersWidth = 51;
+            this.dgvInformacion.RowTemplate.Height = 24;
+            this.dgvInformacion.Size = new System.Drawing.Size(490, 380);
+            this.dgvInformacion.TabIndex = 0;
+            // 
+            // txtDni
+            // 
+            this.txtDni.Location = new System.Drawing.Point(53, 210);
+            this.txtDni.Multiline = true;
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(172, 30);
+            this.txtDni.TabIndex = 1;
+            // 
+            // txtSector
+            // 
+            this.txtSector.Location = new System.Drawing.Point(53, 146);
+            this.txtSector.Multiline = true;
+            this.txtSector.Name = "txtSector";
+            this.txtSector.Size = new System.Drawing.Size(172, 30);
+            this.txtSector.TabIndex = 2;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(53, 82);
+            this.txtNombre.Multiline = true;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(172, 34);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(56, 269);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(167, 45);
+            this.btnGuardar.TabIndex = 4;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(53, 63);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(56, 16);
+            this.lblNombre.TabIndex = 5;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblSector
+            // 
+            this.lblSector.AutoSize = true;
+            this.lblSector.Location = new System.Drawing.Point(53, 127);
+            this.lblSector.Name = "lblSector";
+            this.lblSector.Size = new System.Drawing.Size(46, 16);
+            this.lblSector.TabIndex = 6;
+            this.lblSector.Text = "Sector";
+            // 
+            // lblDni
+            // 
+            this.lblDni.AutoSize = true;
+            this.lblDni.Location = new System.Drawing.Point(53, 191);
+            this.lblDni.Name = "lblDni";
+            this.lblDni.Size = new System.Drawing.Size(30, 16);
+            this.lblDni.TabIndex = 7;
+            this.lblDni.Text = "DNI";
+            // 
+            // frmGrilla
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDni);
+            this.Controls.Add(this.lblSector);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtSector);
+            this.Controls.Add(this.txtDni);
+            this.Controls.Add(this.dgvInformacion);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "frmGrilla";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Grilla de Datos";
+            this.Load += new System.EventHandler(this.frmGrilla_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInformacion)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvInformacion;
+        private System.Windows.Forms.TextBox txtDni;
+        private System.Windows.Forms.TextBox txtSector;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblSector;
+        private System.Windows.Forms.Label lblDni;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Collections.Specialized.BitVector32;
+
+namespace pryLopezSantiagoLab2
+{
+    public partial class frmGrilla : Form
+    {
+        string[,] MatDGV = new string[10, 3];
+        int filaActual = 1;
+        string Nombre = "Nombre";
+        string Sector = "Sector";
+        string DNI = "DNI";
+        public frmGrilla()
+        {
+            InitializeComponent();
+        }
+        private void frmGrilla_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string Nombre = txtNombre.Text;
+            string Sector = txtSector.Text;
+            string Dni = txtDni.Text;
+
+            // Verificar que la caja de texto no esté vacía
+            if (!string.IsNullOrWhiteSpace(Nombre)&& !string.IsNullOrWhiteSpace(Sector) && !string.IsNullOrWhiteSpace(Sector))
+            {
+                // Guardar el valor en la matriz en la posición [0, 0]
+                dgvInformacion.Rows[0].Cells[0].Value = Nombre;
+                dgvInformacion.Rows[0].Cells[1].Value = Sector;
+                dgvInformacion.Rows[0].Cells[2].Value = DNI;
+
+                MatDGV[0, 0] = Nombre;
+                MatDGV[0, 1] = Sector;
+                MatDGV[0, 2] = Dni;
+                txtNombre.Text = "";
+                txtSector.Text = "";
+                txtDni.Text = "";
+                dgvInformacion.Rows[filaActual].Cells[0].Value=MatDGV[filaActual,0];
+                dgvInformacion.Rows[filaActual].Cells[1].Value=MatDGV[filaActual, 1];
+                dgvInformacion.Rows[filaActual].Cells[2].Value=MatDGV[filaActual, 2];
+                filaActual++;
+                if (filaActual >= 10)
+                {
+                    MessageBox.Show("No hay mas espacio.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor completa los campos");
+            }
+
+        }
+    }
+}
